22d43666609daeca2ad23f3f784384d6495470c4
osbm <osmanfbayram@gmail.com>
2025-05-30 23:40:19 +0300

    add initial package.nix for AnkiDroid APK build configuration

---
+++ apks/an/ankidroid/package.nix
@@ -0,0 +1,60 @@
+{pkgs, ...}:
+
+
+let
+  buildToolsVersion = "34.0.0";
+  platformVersion = "35";
+  systemImageType = "google_apis";
+  androidComposition = pkgs.androidenv.composeAndroidPackages {
+    buildToolsVersions = [ buildToolsVersion ];
+    platformToolsVersion = "35.0.2";
+    platformVersions = [ platformVersion ];
+    emulatorVersion = "35.2.5";
+    includeEmulator = true;
+    abiVersions = [ "armeabi-v7a" "arm64-v8a" "x86_64" ];
+    extraLicenses = [
+      "android-googletv-license"
+      "android-sdk-arm-dbt-license"
+      "android-sdk-license"
+      "android-sdk-preview-license"
+      "google-gdk-license"
+      "intel-android-extra-license"
+      "intel-android-sysimage-license"
+      "mips-android-sysimage-license"
+    ];
+  };
+in
+
+pkgs.stdenv.mkDerivation rec {
+  name = "ankidroid-${version}.apk";
+  version = "2.20.1";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "ankidroid";
+    repo = "Anki-Android";
+    tag = "v${version}";
+    hash = "sha256-CPCczLovuQTjUTWgjVjgZ8PL4idlcTzWZ2wKwqsF+cg=";
+  };
+
+  nativeBuildInputs = [
+    androidComposition.androidsdk
+    (with pkgs; [
+      gradle
+      temurin-bin
+      keepBuildTree
+    ])
+  ];
+
+  ANDROID_HOME = "${androidComposition.androidsdk}/libexec/android-sdk";
+  gradleFlags = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_HOME}/build-tools/${buildToolsVersion}/aapt2";
+
+  buildPhase = ''
+    gradle assembleRelease --info --offline --full-stacktrace ${gradleFlags}
+  '';
+
+  installPhase = ''
+    cp app/build/outputs/apk/release/app-release.apk $out
+  '';
+
+
+}
